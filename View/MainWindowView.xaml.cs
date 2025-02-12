887c571f39ee008078ae0dccca17dd293411e24b
ZZ22HohnholdJohannes <Johannes.Hohnhold@bs-erlangen.de>
2025-02-12 10:31:01 +0100

    Startup fixed

--- View/MainWindowView.xaml.cs
+++ View/MainWindowView.xaml.cs
@@ -1,29 +1,26 @@
 ﻿using System.Windows;
 using Reserve_iT.ViewModel;
+using Reserve_iT.View;
 
 namespace Reserve_iT.View
 {
-	/// <summary>
-	/// Interaktionslogik für MainWindowView.xaml
-	/// </summary>
-	public partial class MainWindowView : Window
-	{
-		public MainWindowView()
-		{
-			InitializeComponent();
-		}
-
-		private void Window_Loaded(object sender, RoutedEventArgs e)
-		{
-			var view = new DashboardView();
-			view.DataContext = DataContext;
-			((MainWindowViewModel)DataContext).MainFrame = MainFrame; // Mainframe aus XAML in ViewModel durchreichen
-			MainFrame.Navigate(view);
-		}
-
-    private void MainFrame_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
+  /// <summary>
+  /// Interaktionslogik für MainWindowView.xaml
+  /// </summary>
+  public partial class MainWindowView : Window
+  {
+    public MainWindowView()
     {
-
-        }
+      InitializeComponent();
+      DataContext = new MainWindowViewModel();
     }
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+      var view = new DashboardView();
+      view.DataContext = DataContext;
+      ((MainWindowViewModel)DataContext).MainFrame = MainFrame; // Mainframe aus XAML in ViewModel durchreichen
+      MainFrame.Navigate(view);
+    }
+  }
 }
