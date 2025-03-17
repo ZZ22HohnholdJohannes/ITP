764c92272213b8708eac7d426e238a75fbb012f1
parzivalwhy <alex.hellmuth@bs-erlangen.de>
2025-03-17 14:35:45 +0100

    Update Setup_Complete.sql

--- SQL/Setup_Complete.sql
+++ SQL/Setup_Complete.sql
@@ -474,7 +474,7 @@ BEGIN
 	
 	SELECT COUNT(*) INTO auftrag_id_gueltig
 	FROM auftrag
-	WHERE auftrag_ID = auftrag_id_in
+	WHERE auftrag_ID = auftrag_id_in;
 	
 	if auftrag_id_vorhanden = 0 AND auftrag_id_gueltig > 0 THEN
 	
