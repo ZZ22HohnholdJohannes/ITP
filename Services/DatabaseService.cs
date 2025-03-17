d1a04f445fcd794a10bd949481589da88efa8972
ZZ22HohnholdJohannes <Johannes.Hohnhold@bs-erlangen.de>
2025-03-17 08:32:23 +0100

    databseService changed
    There is only one connection string in the databaseService. The other Services are calling the databaseService to get the data from db

---
+++ Services/DatabaseService.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows;
+using Reserve_iT.Services;
+using MySql.Data.MySqlClient;
+
+namespace Reserve_iT.Services
+{
+  public class DatabaseService
+  {
+    private static readonly string ConnectionString;
+
+    static DatabaseService()
+    {
+      ConnectionString = new MySqlConnectionStringBuilder()
+      {
+        Server = "127.0.0.1",
+        Database = "reserve_it",
+        UserID = "root",
+        Password = "",
+        SslMode = MySqlSslMode.Disabled
+      }.ConnectionString;
+    }
+
+    public static DataTable ExecuteSP(string sp)
+    {
+      DataTable tbl = new DataTable();
+      try
+      {
+        using (MySqlConnection connection = new MySqlConnection(ConnectionString))
+        {
+          MySqlCommand command = new MySqlCommand(sp, connection)
+          {
+            CommandType = CommandType.StoredProcedure
+          };
+
+          using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+          {
+            adapter.Fill(tbl);
+          }
+        }
+      }
+      catch (Exception ex)
+      {
+        MessageBoxService.DisplayMessage(ex.Message, MessageBoxImage.Error);
+      }
+      return tbl;
+    }
+
+    public static DataTable ExecuteSP(string sp, Dictionary<string, object> parameters)
+    {
+      DataTable tbl = new DataTable();
+      try
+      {
+        using (MySqlConnection connection = new MySqlConnection(ConnectionString))
+        {
+          MySqlCommand command = new MySqlCommand(sp, connection)
+          {
+            CommandType = CommandType.StoredProcedure
+          };
+          foreach (KeyValuePair<string, object> item in parameters)
+          {
+            command.Parameters.AddWithValue(item.Key, item.Value ?? DBNull.Value);
+          }
+          using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+          {
+            adapter.Fill(tbl);
+
+            // Fehlermeldungen abfangen
+            if (tbl is not null && tbl.Rows.Count == 1)
+              if (tbl.Columns.Contains("Nachricht") && tbl.Rows[0]["Nachricht"] != DBNull.Value)
+                MessageBoxService.DisplayMessage(tbl.Rows[0]["Nachricht"].ToString() ?? "DB-Fehler", MessageBoxImage.Warning);
+          }
+        }
+      }
+      catch (Exception ex)
+      {
+        MessageBoxService.DisplayMessage(ex.Message, MessageBoxImage.Error);
+      }
+      return tbl;
+    }
+
+    public static DataSet ExecuteSPDataSet(string sp, Dictionary<string, object> parameters)
+    {
+      DataSet dataSet = new DataSet();
+      try
+      {
+        using (MySqlConnection connection = new MySqlConnection(ConnectionString))
+        {
+          MySqlCommand command = new MySqlCommand(sp, connection)
+          {
+            CommandType = CommandType.StoredProcedure
+          };
+
+          foreach (KeyValuePair<string, object> item in parameters)
+          {
+            command.Parameters.AddWithValue(item.Key, item.Value ?? DBNull.Value);
+          }
+
+          using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+          {
+            adapter.Fill(dataSet);
+          }
+        }
+      }
+      catch (Exception ex)
+      {
+        MessageBoxService.DisplayMessage(ex.Message, MessageBoxImage.Error);
+      }
+      return dataSet;
+    }
+
+    public static DataSet ExecuteSPDataSet(string sp)
+    {
+      DataSet dataSet = new DataSet();
+      try
+      {
+        using (MySqlConnection connection = new MySqlConnection(ConnectionString))
+        {
+          MySqlCommand command = new MySqlCommand(sp, connection)
+          {
+            CommandType = CommandType.StoredProcedure
+          };
+
+          using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+          {
+            adapter.Fill(dataSet);
+          }
+        }
+      }
+      catch (Exception ex)
+      {
+        MessageBoxService.DisplayMessage(ex.Message, MessageBoxImage.Error);
+      }
+      return dataSet;
+    }
+  }
+}
---
+++ Services/MessageBoxService.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace Reserve_iT.Services
+{
+  public static class MessageBoxService
+  {
+    public static void DisplayMessage(string message, MessageBoxImage image)
+    {
+      string title = image switch
+      {
+        MessageBoxImage.Error => "Fehler",
+        MessageBoxImage.Warning => "Warnung",
+        MessageBoxImage.Information => "Information",
+        _ => "Meldung"
+      };
+
+      if (image == MessageBoxImage.Error)
+        message = $"Es ist ein Fehler aufgetreten: {message}";
+
+      MessageBox.Show(message, title, MessageBoxButton.OK, image);
+    }
+  }
+}
