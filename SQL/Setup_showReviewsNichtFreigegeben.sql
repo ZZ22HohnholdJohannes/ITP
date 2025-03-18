be01e41ed64a8cb3d4d35d6d5f17fe1dbba3eb14
parzivalwhy <alex.hellmuth@gmx.de>
2025-03-18 16:51:31 +0100

    upsi3

--- SQL/Setup_showReviewsNichtFreigegeben.sql
+++ SQL/Setup_showReviewsNichtFreigegeben.sql
@@ -4,11 +4,11 @@ DELIMITER //
 CREATE PROCEDURE showReviewsNichtFreigegeben()
 BEGIN 
 	
-	SELECT b.bewertung_ID
-			,a.auftrag_ID
-			,g.vorname
-			,g.nachname
-			,b.rezension
+	SELECT b.bewertung_ID AS bewertung_ID
+			,a.auftrag_ID AS auftrag_ID
+			,g.vorname AS Vorname
+			,g.nachname AS Nachname
+			,b.rezension AS rezension
 	FROM bewertung b
 	JOIN auftrag a ON a.auftrag_ID = b.auftrag_ID
 	JOIN gast g ON g.gast_ID = a.gast_ID
